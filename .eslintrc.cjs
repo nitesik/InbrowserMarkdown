f7135883a5aba7ef462063fde3b8c3c97c982c21
nitesik <neeteshchhetri69@gmail.com>
2023-05-02 00:34:42 +0530

    bugs fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,8 @@ const config = {
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off"
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
