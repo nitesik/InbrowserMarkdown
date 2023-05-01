a476d8e13ff7ca31b6f34749e3ef3ac8ee08b487
nitesik <neeteshchhetri69@gmail.com>
2023-05-02 00:35:40 +0530

    Updates

--- static/9LGLCp3swxdSZpbwy24zk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/636-bd3b081602c7eab5.js","static/css/e874835f2ef52111.css","static/chunks/pages/index-04f8d44aeee89ed7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
